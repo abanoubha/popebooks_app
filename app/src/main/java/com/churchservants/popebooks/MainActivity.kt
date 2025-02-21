d735d2ab7f21210271a596d51dad51a964510112
Abanoub Hanna <abanoubhannaa73@gmail.com>
2025-02-22 00:39:24 +0200

    refactor: remove commented out code

--- app/src/main/java/com/churchservants/popebooks/MainActivity.kt
+++ app/src/main/java/com/churchservants/popebooks/MainActivity.kt
@@ -4,13 +4,10 @@ import android.database.sqlite.SQLiteDatabase
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.animation.core.animateFloatAsState
-import androidx.compose.animation.core.tween
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.gestures.detectHorizontalDragGestures
 import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.WindowInsets
@@ -40,7 +37,6 @@ import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableFloatStateOf
 import androidx.compose.runtime.mutableIntStateOf
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
@@ -48,7 +44,6 @@ import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.graphics.graphicsLayer
 import androidx.compose.ui.input.pointer.pointerInput
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.res.stringResource
@@ -72,10 +67,6 @@ class MainActivity : ComponentActivity() {
                         .windowInsetsPadding(WindowInsets.safeDrawing)
                 ) { innerPadding ->
                     BookListScreen(modifier = Modifier.padding(innerPadding))
-//                    FlippingText(
-//                        text = "Hello, Word!",
-//                        modifier = Modifier.padding(innerPadding),
-//                    )
                 }
             }
         }
@@ -340,40 +331,4 @@ fun loadPageContent(db: SQLiteDatabase, bookId: Int, pageNumber: Int): String? {
         }
     }
     return null
-}
-
-@Composable
-fun FlippingText(
-    text: String,
-    modifier: Modifier = Modifier,
-    style: TextStyle = TextStyle(fontSize = 20.sp)
-) {
-    var rotation by remember { mutableFloatStateOf(0f) }
-    var targetRotation by remember { mutableFloatStateOf(0f) }
-
-    val animatedRotation by animateFloatAsState(
-        targetValue = targetRotation,
-        animationSpec = tween(durationMillis = 300)
-    )
-
-    Box(modifier = modifier) {
-        Text(
-            text = text,
-            style = style,
-            modifier = Modifier
-                .graphicsLayer {
-                    rotationX = animatedRotation
-                    cameraDistance = 8f // Adjust for 3D effect depth
-                }
-                .pointerInput(Unit) {
-                    detectHorizontalDragGestures { _, dragAmount ->
-                        if (dragAmount > 0) { // Swiping right
-                            targetRotation -= 180f // Flip 180 degrees
-                        } else if (dragAmount < 0) { // Swiping left
-                            targetRotation += 180f // Flip 180 degrees
-                        }
-                    }
-                }
-        )
-    }
 }
