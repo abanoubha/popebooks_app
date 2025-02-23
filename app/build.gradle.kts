ff31f0f61f8884a04453f0fdd2c63f920959f86c
Abanoub Hanna <abanoubhannaa73@gmail.com>
2025-02-23 20:55:36 +0200

    build: set debuggability

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,6 +20,7 @@ android {
 
     buildTypes {
         release {
+            isDebuggable = false
             isMinifyEnabled = false
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
